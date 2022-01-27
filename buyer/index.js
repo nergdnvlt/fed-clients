5a393984cd9c898c4064a7955680309fee814db8
Tyler Lundgren <tylerlundgren@yahoo.com>
2022-01-26 16:02:43 -0800

    Updating buyer client to reflect current source code.

--- buyer/index.js
+++ buyer/index.js
@@ -1,188 +1,184 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-async function fetchAllProducts(randomClient, randomVersion) {
-  const response = await fetch(
-    "https://prod-fed-gateway-waaq4qt37q-uc.a.run.app",
-    {
-      method: "POST",
+exports.helloPubSub = (event, context) => {
+  async function fetchAllProducts(randomClient, randomVersion) {
+    const response = await fetch('https://prod-fed-gateway-waaq4qt37q-uc.a.run.app', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        "apollographql-client-name": randomClient,
-        "apollographql-client-version": randomVersion,
+        'Content-Type': 'application/json',
+        'apollographql-client-name': randomClient,
+        'apollographql-client-version': randomVersion
       },
-      body: JSON.stringify({
-        query: `query getAllProducts {
-            products {
-                name
-                brand
-                price {
-                usdPrice
-                priceDecorator
-                }
-            }
-        }`,
-      }),
-    }
-  )
+      body:JSON.stringify({ query: `query getAllProducts {
+        products {
+          name
+          brand
+          price {
+            usdPrice
+            priceDecorator
+          }
+        }
+      }`})
+    })
     .then((res) => res.json())
     .then((result) => {
-      console.log(result.data);
-      return result.data;
+      console.log(result.data)
+      return result.data
     });
-  return response;
-}
+    return response
+  };
 
-async function fetchOneProduct(randomClient, randomVersion, randomProduct) {
-  const response = await fetch(
-    "https://prod-fed-gateway-waaq4qt37q-uc.a.run.app",
-    {
-      method: "POST",
+  async function fetchOneProduct(randomClient, randomVersion, randomProduct) {
+    const response = await fetch('https://prod-fed-gateway-waaq4qt37q-uc.a.run.app', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        "apollographql-client-name": randomClient,
-        "apollographql-client-version": randomVersion,
+        'Content-Type': 'application/json',
+        'apollographql-client-name': randomClient,
+        'apollographql-client-version': randomVersion
       },
-      body: JSON.stringify({
+      body:JSON.stringify({ 
         query: `query getOneProduct($productId: ID!) {
-            product(id: $productId) {
+          product(id: $productId) {
+            name
+            brand
+            description
+            color
+            price {
+              usdPrice
+              priceDecorator
+            }
+          }
+        }`,
+        variables: {
+          productId: randomProduct
+        }
+      })
+    })
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result.data)
+      return result.data
+    });
+    return response
+  };
+
+  async function fetchUserAccount(randomClient, randomVersion, randomUser) {
+    const response = await fetch('https://prod-fed-gateway-waaq4qt37q-uc.a.run.app', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'apollographql-client-name': randomClient,
+        'apollographql-client-version': randomVersion
+      },
+      body:JSON.stringify({ 
+        query: `query getAccountInfo($userId: ID!) {
+          user(id: $userId) {
+            firstName
+            lastName
+            address
+            email
+            orders {
+              products {
                 name
-                brand
-                description
-                color
                 price {
-                    usdPrice
-                    priceDecorator
+                  usdPrice
                 }
+              }
             }
+          }
         }`,
         variables: {
-          productId: randomProduct,
-        },
-      }),
-    }
-  )
+          userId: randomUser
+        }
+      })
+    })
     .then((res) => res.json())
     .then((result) => {
-      console.log(result.data);
-      return result.data;
+        console.log(result.data)
+        return result.data
     });
-  return response;
-}
+    return response
+  }; 
 
-async function fetchUserAccount(randomClient, randomVersion, randomUser) {
-  const response = await fetch(
-    "https://prod-fed-gateway-waaq4qt37q-uc.a.run.app",
-    {
-      method: "POST",
+  async function legacyClientAccount(randomUser) {
+    const response = await fetch('https://prod-fed-gateway-waaq4qt37q-uc.a.run.app', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        "apollographql-client-name": randomClient,
-        "apollographql-client-version": randomVersion,
+        'Content-Type': 'application/json',
+        'apollographql-client-name': 'iOS-buyer',
+        'apollographql-client-version': '0.8-legacy'
       },
-      body: JSON.stringify({
+      body:JSON.stringify({ 
         query: `query getAccountInfo($userId: ID!) {
-            user(id: $userId) {
-                firstName
-                lastName
-                address
-                email
-                orders {
-                    products {
-                        name
-                        price {
-                            usdPrice
-                        }
-                    }
+          user(id: $userId) {
+            firstName
+            lastName
+            address
+            username
+            orders {
+              products {
+                name
+                price {
+                  usdPrice
                 }
+              }
             }
+          }
         }`,
         variables: {
-          userId: randomUser,
-        },
-      }),
-    }
-  )
+          userId: randomUser
+        }
+      })
+    })
     .then((res) => res.json())
     .then((result) => {
-      console.log(result.data);
-      return result.data;
+        console.log(result.data)
+        return result.data
     });
-  return response;
-}
+    return response
+  }; 
 
-async function legacyClientAccount(randomUser) {
-  const response = await fetch(
-    "https://prod-fed-gateway-waaq4qt37q-uc.a.run.app",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "apollographql-client-name": "iOS-buyer",
-        "apollographql-client-version": "0.8-legacy",
-      },
-      body: JSON.stringify({
-        query: `query getAccountInfo($userId: ID!) {
-            user(id: $userId) {
-                firstName
-                lastName
-                address
-                username
-                orders {
-                    products {
-                        name
-                        price {
-                            usdPrice
-                        }
-                    }
-                }
-            }
-        }`,
-        variables: {
-          userId: randomUser,
-        },
-      }),
-    }
-  )
-    .then((res) => res.json())
-    .then((result) => {
-      console.log(result.data);
-      return result.data;
-    });
-  return response;
-}
 
-let clients = ["iOS-buyer", "Web-buyer", "Android-buyer"];
-let randomClient = clients[Math.floor(Math.random() * clients.length)];
 
-let versions = ["1.0", "2.0"];
+  let clients = [
+    'iOS-buyer',
+    'Web-buyer',
+    'Android-buyer'
+  ]
+  let randomClient = clients[Math.floor(Math.random() * clients.length)];
 
-let randomVersion = versions[Math.floor(Math.random() * versions.length)];
+  let versions = [
+    '1.0',
+    '2.0'
+  ]
+  let randomVersion = versions[Math.floor(Math.random() * versions.length)];
 
-let oddsArray = [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4];
-let random = oddsArray[Math.floor(Math.random() * oddsArray.length)];
+  let oddsArray = [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4 ]
+  let random = oddsArray[Math.floor(Math.random() * oddsArray.length)];
 
-let randomProduct = Math.floor(Math.random() * 15 + 1);
+  let randomProduct = Math.floor(Math.random() * 15 + 1);
 
-let randomUser = Math.floor(Math.random() * 8 + 1);
+  let randomUser = Math.floor(Math.random() * 8 + 1);
 
-// let possibleTimes = [...Array(300).keys()].map(x => x + 30 )
-let possibleTimes = [...Array(10).keys()].map((x) => x + 2);
-let times = possibleTimes[Math.floor(Math.random() * possibleTimes.length)];
+  // let possibleTimes = [...Array(300).keys()].map(x => x + 30 )
+  let possibleTimes = [...Array(10).keys()].map(x => x + 2 )
+  let times = possibleTimes[Math.floor(Math.random() * possibleTimes.length)];
 
-if (random === 1) {
-  for (let i = 0; i < times; i++) {
-    fetchAllProducts(randomClient, randomVersion);
-  }
-} else if (random === 2) {
-  for (let i = 0; i < times; i++) {
-    fetchOneProduct(randomClient, randomVersion, randomProduct);
-  }
-} else if (random === 3) {
-  for (let i = 0; i < times; i++) {
-    fetchUserAccount(randomClient, randomVersion, randomUser);
-  }
-} else {
-  for (let i = 0; i < 4; i++) {
-    legacyClientAccount(randomUser);
-  }
-}
+  if(random === 1) {
+      for (let i = 0; i < times; i++) {
+          fetchAllProducts(randomClient, randomVersion);
+      }
+  } else if (random === 2) {
+      for (let i = 0; i < times; i++) {
+          fetchOneProduct(randomClient, randomVersion, randomProduct);
+      }
+  } else if (random === 3) {
+      for (let i = 0; i < times; i++) {
+          fetchUserAccount(randomClient, randomVersion, randomUser);
+      } 
+  } else {
+      for (let i = 0; i < 4; i++) {
+          legacyClientAccount(randomUser);
+      }
+  };
+};
