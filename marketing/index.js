69cc64213ff2b3a57b7ab2f56d0938786fc4aa59
Tyler Lundgren <tylerlundgren@yahoo.com>
2022-01-26 16:03:52 -0800

    Updating Marketing for current google function source.

--- marketing/index.js
+++ marketing/index.js
@@ -1,51 +1,52 @@
 const fetch = require('node-fetch');
 
-async function fetchMarketingInfo(randomClient, randomVersion) {
-  const response = await fetch('https://marketing-waaq4qt37q-uc.a.run.app', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'apollographql-client-name': randomClient,
-      'apollographql-client-version': randomVersion
-    },
-    body:JSON.stringify({ query: `query getMarketingInfo {
-      users {
-        firstName
-        lastName
-        username
-        orders {
-          products {
-            name
+exports.helloPubSub = (event, context) => {
+  async function fetchMarketingInfo(randomClient, randomVersion) {
+    const response = await fetch('https://marketing-waaq4qt37q-uc.a.run.app', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'apollographql-client-name': randomClient,
+        'apollographql-client-version': randomVersion
+      },
+      body:JSON.stringify({ query: `query getMarketingInfo {
+        users {
+          firstName
+          lastName
+          username
+          orders {
+            products {
+              name
+            }
           }
         }
-      }
-    }`
+      }`
+      })
     })
-  })
-  .then((res) => res.json())
-  .then((result) => {
-    console.log(result.data)
-    return result.data
-  });
-    return response
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result.data)
+      return result.data
+    });
+      return response
+  };
+
+  let clients = [
+      'Scraper-marketing',
+      'API-marketing',
+  ]
+  let randomClient = clients[Math.floor(Math.random() * clients.length)];
+
+  let versions = [
+    '1.0',
+    '2.0',
+  ]
+  let randomVersion = versions[Math.floor(Math.random() * versions.length)];
+
+  let possibleTimes = [...Array(30).keys()].map(x => x + 1 )
+  let times = possibleTimes[Math.floor(Math.random() * possibleTimes.length)];
+
+  for (let i = 0; i < times; i++) {
+      fetchMarketingInfo(randomClient, randomVersion);
+  }
 };
-
-let clients = [
-    'Scraper-marketing',
-    'API-marketing',
-]
-let randomClient = clients[Math.floor(Math.random() * clients.length)];
-
-let versions = [
-  '1.0',
-  '1.1',
-  '2.0',
-]
-let randomVersion = versions[Math.floor(Math.random() * versions.length)];
-
-let possibleTimes = [...Array(30).keys()].map(x => x + 1 )
-let times = possibleTimes[Math.floor(Math.random() * possibleTimes.length)];
-
-for (let i = 0; i < times; i++) {
-    fetchMarketingInfo(randomClient, randomVersion);
-}
